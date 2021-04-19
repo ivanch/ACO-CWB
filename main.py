ad390576dc4b3f8dd1d682128aaef0147695376b
Heitor Ventura <heitorventura21@gmail.com>
2021-04-19 21:46:04 +0000

    returned with the stochastic cost

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def on_iteration_completed(best_path, best_distance):
 if __name__ == '__main__':
   graph = graph_from_file('nodes.csv')
 
-  aco = ACO(graph = graph, ants = 1000, evaporation_rate = 0.1, intensification = 1, choose_best = 0.15)
+  aco = ACO(graph = graph, ants = 500, evaporation_rate = 0.1, intensification = 1, choose_best = 0.15)
   best_path, best_distance = aco.run(iterations = 20, on_iteration_completed = on_iteration_completed)
 
   plt.show()
